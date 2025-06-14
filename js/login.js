25f328df78e7f14854590fa1af71858a5649e676
selim2066 <121637755+selim2066@users.noreply.github.com>
2025-06-14 13:55:36 +0600

    add money section continue..

---
+++ js/login.js
@@ -0,0 +1,18 @@
+console.log("whts up dear")
+document.getElementById('loginBtn').addEventListener('click', function (event) {
+    console.log("hi")
+    event.preventDefault()
+    const phoneNum = document.getElementById('phoneNum')
+    const pin = document.getElementById('pin')
+
+    if (phoneNum.value === '2066' && pin.value === '213') {
+        console.log('good, this is your new home')
+        window.location.href='/home.html'
+
+    } else {
+        alert('wrong credential\n')
+    }
+    phoneNum.value = ''
+    pin.value = ''
+
+})
